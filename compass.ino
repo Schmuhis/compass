90054c518ebedb41ea19271c531db79e494fc986
majafranz <majafranz99@gmail.com>
2025-05-23 16:37:47 +0200

    use quartal

--- compass.ino
+++ compass.ino
@@ -1,8 +1,8 @@
 #include <Adafruit_NeoPixel.h>
-#include <QMC5883LCompass.h>
-#include <math.h>
-#include <WebSocketsClient.h>
 #include <ArduinoJson.h>
+#include <QMC5883LCompass.h>
+#include <WebSocketsClient.h>
+#include <math.h>
 
 #define RING_PIN 12
 #define NUMPIXELS 12
@@ -13,7 +13,7 @@
 #define RADIANS 57.2958
 
 Adafruit_NeoPixel pixels =
-  Adafruit_NeoPixel(NUMPIXELS, RING_PIN, NEO_GRB + NEO_KHZ800);
+    Adafruit_NeoPixel(NUMPIXELS, RING_PIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t white = pixels.Color(255, 255, 255);
 uint32_t red = pixels.Color(255, 0, 0);
@@ -22,24 +22,23 @@ uint32_t blue = pixels.Color(0, 0, 255);
 uint32_t snake_colour = pixels.Color(144, 1, 122);
 uint32_t background_colour = pixels.Color(232, 128, 30);
 
-const char* GAME_PHASE = "UNKOWN";
-const char* UUID = "db1a60b8-02dd-47cf-8a7d-40f2bfc4f33e";
-const char* CONNECT_ID = "espCompass_db1a60b8_connect";
+const char *GAME_PHASE = "UNKOWN";
+const char *UUID = "db1a60b8-02dd-47cf-8a7d-40f2bfc4f33e";
+const char *CONNECT_ID = "espCompass_db1a60b8_connect";
 uint32_t i, j = 0, k;
 uint32_t compass_interval;
-const char* CONNECTION_STATE = "UNKNOWN";
+const char *CONNECTION_STATE = "UNKNOWN";
 
-const char* ssid = "Techbase Guest";
-const char* password = "Hackaburg25";
-const char* websocket_server = "hide-and-seek-unxw.onrender.com";  // Example secure WebSocket server
+const char *ssid = "Techbase Guest";
+const char *password = "Hackaburg25";
+const char *websocket_server =
+    "hide-and-seek-unxw.onrender.com"; // Example secure WebSocket server
 // const char* websocket_server = "echo.websocket.events";
 
-
-float hider_lat = 20;
-float hider_long = 20;
-float seeker_lat = 30;
-float seeker_long = 50;
-
+float hider_lat = 10;
+float hider_long = 0;
+float seeker_lat = 0;
+float seeker_long = 0;
 
 QMC5883LCompass compass;
 WebSocketsClient webSocket;
@@ -59,23 +58,31 @@ void sendCompassMoinRequest() {
 }
 
 void handleUpdateStateEvent(JsonDocument updateEvent) {
-  
-  if(updateEvent["message"]["value"]["state"]["room"]["gamePhase"] == "lobby"){ //change to seeking for prod
-    String hiderId = updateEvent["message"]["value"]["state"]["room"]["hiderId"];
-    String hiderPosition = updateEvent["message"]["value"]["state"]["room"]["positions"];
-    hider_long = updateEvent["message"]["value"]["state"]["room"]["positions"][hiderPosition]["long"];
-    hider_lat = updateEvent["message"]["value"]["state"]["room"]["positions"][hiderPosition]["lat"];
-    seeker_long = updateEvent["message"]["value"]["state"]["room"]["positions"][UUID]["long"];
-    seeker_lat = updateEvent["message"]["value"]["state"]["room"]["positions"][UUID]["lat"];
+
+  if (updateEvent["message"]["value"]["state"]["room"]["gamePhase"] ==
+      "lobby") { // change to seeking for prod
+    String hiderId =
+        updateEvent["message"]["value"]["state"]["room"]["hiderId"];
+    String hiderPosition =
+        updateEvent["message"]["value"]["state"]["room"]["positions"];
+    hider_long = updateEvent["message"]["value"]["state"]["room"]["positions"]
+                            [hiderPosition]["long"];
+    hider_lat = updateEvent["message"]["value"]["state"]["room"]["positions"]
+                           [hiderPosition]["lat"];
+    seeker_long = updateEvent["message"]["value"]["state"]["room"]["positions"]
+                             [UUID]["long"];
+    seeker_lat = updateEvent["message"]["value"]["state"]["room"]["positions"]
+                            [UUID]["lat"];
     Serial.println("Updated Location Stats");
   }
 }
 
-void handleResponse(uint8_t* payload) {
+void handleResponse(uint8_t *payload) {
   JsonDocument doc;
   deserializeJson(doc, payload);
   if (doc["id"] == CONNECT_ID && CONNECTION_STATE != "CONNECTED") {
-    Serial.println("Received same connect id, switching connection state to 'CONNECTED'");
+    Serial.println(
+        "Received same connect id, switching connection state to 'CONNECTED'");
     CONNECTION_STATE = "CONNECTED";
   }
 
@@ -84,25 +91,25 @@ void handleResponse(uint8_t* payload) {
   }
 }
 
-void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
   switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.println("WebSocket Disconnected");
-      break;
-    case WStype_CONNECTED:
-      sendCompassMoinRequest();
-      break;
-    case WStype_TEXT:
-      // Serial.printf("Received: %s\n", payload);
-      handleResponse(payload);
+  case WStype_DISCONNECTED:
+    Serial.println("WebSocket Disconnected");
+    break;
+  case WStype_CONNECTED:
+    sendCompassMoinRequest();
+    break;
+  case WStype_TEXT:
+    // Serial.printf("Received: %s\n", payload);
+    handleResponse(payload);
 
-      break;
+    break;
   }
 }
 void init_compass() {
   compass.init();
-  compass.setCalibrationOffsets(-161.00, 515.00, -503.00);
-  compass.setCalibrationScales(0.87, 0.85, 1.50);
+  compass.setCalibrationOffsets(-974.00, -1765.00, 8.00);
+  compass.setCalibrationScales(1.28, 0.71, 1.25);
 }
 
 void init_LED_ring() {
@@ -123,13 +130,13 @@ void setup() {
   }
   Serial.println("\nWiFi connected");
 
-  webSocket.beginSSL(websocket_server, 443, "/api/websocket");  // WSS
+  webSocket.beginSSL(websocket_server, 443, "/api/websocket"); // WSS
   webSocket.onEvent(webSocketEvent);
   webSocket.setReconnectInterval(5000);
 }
 
 uint32_t get_compass_interval(int azimuth) {
-  unsigned long a = (azimuth > -0.5) ? azimuth / 30.0 : (azimuth + 360) / 30.0;
+  unsigned long a = (azimuth > -0.5) ? azimuth / 90.0 : (azimuth + 360) / 90.0;
   unsigned long r = a - (int)a;
   byte sexdec = 0;
   sexdec = (r >= .5) ? ceil(a) : floor(a);
@@ -142,15 +149,13 @@ float get_lat_direction(float my_lat, float other_lat) {
 
 float get_long_direction(float my_long, float other_long) {
   return other_long - my_long;
-}  //
+} //
 
 void set_LED_direction(uint32_t compass_interval) {
-  for (i = 0; i < NUMPIXELS; i++) {
-    if (i != compass_interval) {
-      pixels.setPixelColor(i, blue);
-    }
-  }
-  pixels.setPixelColor(compass_interval, red);
+  for (i = 0; i < NUMPIXELS; i++)
+    pixels.setPixelColor(i, blue);
+  for (i = 0; i < 3; i++)
+    pixels.setPixelColor(compass_interval * 3 + i, red);
   pixels.show();
 }
 
@@ -191,7 +196,8 @@ void show_disconnecting_LEDs() {
 }
 
 void handle_LED() {
-  if (CONNECTION_STATE != "CONNECTED") {
+  // if (CONNECTION_STATE != "CONNECTED") {
+  if (1) {
     compass_interval = get_compass_interval_for_dir(hider_lat, hider_long,
                                                     seeker_lat, seeker_long);
     set_LED_direction(compass_interval);
